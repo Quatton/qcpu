5b4e689be3aa43086f1922e5a54a278b9b52844c
Quatton <atomicatives@gmail.com>
2025-02-24 00:59:24 +0900

    i reset hard so many times

--- qcpu_simulator/src/v4/bp.rs
+++ qcpu_simulator/src/v4/bp.rs
@@ -2,6 +2,7 @@ use std::fmt::Display;
 
 use super::syntax::{OpName, OpV4};
 
+#[derive(Debug, Clone, Copy)]
 pub struct BranchPredictor {
     pub taken_pht: [i8; TAKEN_PHT_SIZE],
     pub untaken_pht: [i8; TAKEN_PHT_SIZE],
@@ -9,7 +10,7 @@ pub struct BranchPredictor {
     pub jalr_addr: [usize; JALR_ADDR_SIZE],
     pub gh: usize,
 
-    pub flash_count: [usize; 2],
+    pub flush_count: [usize; 2],
     pub total_count: [usize; 2],
 }
 
@@ -17,9 +18,9 @@ const TAKEN_PHT_SIZE: usize = 1024;
 const TAKEN_PHT_MASK: usize = TAKEN_PHT_SIZE - 1;
 const SELECTOR_PHT_SIZE: usize = 256;
 const SELECTOR_PHT_MASK: usize = SELECTOR_PHT_SIZE - 1;
-const JALR_ADDR_SIZE: usize = 1024;
+const JALR_ADDR_SIZE: usize = 2048;
 const JALR_ADDR_MASK: usize = JALR_ADDR_SIZE - 1;
-const GH_SIZE: usize = 10;
+const GH_SIZE: usize = 1024;
 const GH_MASK: usize = GH_SIZE - 1;
 
 impl BranchPredictor {
@@ -30,7 +31,7 @@ impl BranchPredictor {
             selector_pht: [2; SELECTOR_PHT_SIZE],
             jalr_addr: [0; JALR_ADDR_SIZE],
             gh: 0,
-            flash_count: [0; 2],
+            flush_count: [0; 2],
             total_count: [0; 2],
         }
     }
@@ -52,10 +53,10 @@ impl BranchPredictor {
             }
             OpName::Beq | OpName::Bne | OpName::Blt | OpName::Bge => {
                 let xor = self.gh ^ pci;
-                let taken_idx = xor & TAKEN_PHT_MASK;
                 let selector_idx = xor & SELECTOR_PHT_MASK;
                 let sel = unsafe { *self.selector_pht.get_unchecked(selector_idx) };
                 if sel >= 2 {
+                    let taken_idx = xor & TAKEN_PHT_MASK;
                     let taken_val = unsafe { *self.taken_pht.get_unchecked(taken_idx) };
                     if taken_val >= 2 {
                         taken
@@ -63,7 +64,8 @@ impl BranchPredictor {
                         untaken
                     }
                 } else {
-                    let untaken_val = unsafe { *self.untaken_pht.get_unchecked(taken_idx) };
+                    let untaken_idx = xor & TAKEN_PHT_MASK;
+                    let untaken_val = unsafe { *self.untaken_pht.get_unchecked(untaken_idx) };
                     if untaken_val >= 2 {
                         taken
                     } else {
@@ -88,41 +90,47 @@ impl BranchPredictor {
         match op.opname {
             OpName::Jalr => {
                 self.total_count[0] += 1;
-                unsafe {
-                    *self.jalr_addr.get_unchecked_mut(pci & JALR_ADDR_MASK) = next_pc;
+                let flushed = next_pc != predicted_pc;
+                if flushed {
+                    self.flush_count[0] += 1;
+
+                    unsafe {
+                        *self.jalr_addr.get_unchecked_mut(pci & JALR_ADDR_MASK) = next_pc;
+                    }
                 }
-                let flashed = next_pc != predicted_pc;
-                if flashed {
-                    self.flash_count[0] += 1;
-                }
-                flashed
+                flushed
             }
             OpName::Beq | OpName::Bne | OpName::Blt | OpName::Bge => {
                 self.total_count[1] += 1;
-                self.gh = ((self.gh << 1) | (next_pc != pc + 4) as usize) & GH_MASK;
                 let xor = self.gh ^ pci;
+
+                self.gh = ((self.gh << 1) | (next_pc != pc + 4) as usize) & GH_MASK;
+
+                let flushed = next_pc != predicted_pc;
+
                 let taken_idx = xor & TAKEN_PHT_MASK;
+                let untaken_idx = xor & TAKEN_PHT_MASK;
                 let selector_idx = xor & SELECTOR_PHT_MASK;
 
                 let taken = unsafe { self.taken_pht.get_unchecked_mut(taken_idx) };
-                let untaken = unsafe { self.untaken_pht.get_unchecked_mut(taken_idx) };
+                let untaken = unsafe { self.untaken_pht.get_unchecked_mut(untaken_idx) };
                 let selector = unsafe { self.selector_pht.get_unchecked_mut(selector_idx) };
 
-                if next_pc != predicted_pc {
+                if next_pc != pc.wrapping_add(4) {
                     *taken = (*taken + 1).min(3);
+                    *untaken = (*untaken + 1).min(3);
                     *selector = (*selector + 1).min(3);
                 } else {
                     *untaken = (*untaken - 1).max(0);
+                    *taken = (*taken - 1).max(0);
                     *selector = (*selector - 1).max(0);
                 }
 
-                let flashed = next_pc != predicted_pc;
-
-                if flashed {
-                    self.flash_count[1] += 1;
+                if flushed {
+                    self.flush_count[1] += 1;
                 }
 
-                flashed
+                flushed
             }
             _ => false,
         }
@@ -139,15 +147,15 @@ impl Display for BranchPredictor {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         writeln!(
             f,
-            "JALR Flash Count: {} ({:.02}%)",
-            self.flash_count[0],
-            self.flash_count[0] as f64 / self.total_count[0] as f64 * 100.0
+            "JALR Flush Count: {} ({:.02}%)",
+            self.flush_count[0],
+            self.flush_count[0] as f64 / self.total_count[0] as f64 * 100.0
         )?;
         write!(
             f,
-            "Branch Flash Count: {} ({:.02}%)",
-            self.flash_count[1],
-            self.flash_count[1] as f64 / self.total_count[1] as f64 * 100.0
+            "Branch Flush Count: {} ({:.02}%)",
+            self.flush_count[1],
+            self.flush_count[1] as f64 / self.total_count[1] as f64 * 100.0
         )?;
         Ok(())
     }
