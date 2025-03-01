7d3c7f7b1a4e46b6f11906b14a4107d5074134da
Quatton <atomicatives@gmail.com>
2025-03-02 01:37:51 +0900

    4 words prefetch

--- qcpu_simulator/src/v4/memory.rs
+++ qcpu_simulator/src/v4/memory.rs
@@ -47,7 +47,7 @@ impl CacheLine {
     #[cfg(not(feature = "conflict_pair"))]
     #[inline(always)]
     pub fn replace(&mut self, addr: usize) -> bool {
-        let tag = (addr >> CACHE_LINE_BITS) as u8;
+        let tag = (addr >> CACHE_LINE_BITS >> 2) as u8;
         let hit = self.tag == tag;
         self.tag = tag;
         hit
@@ -65,7 +65,7 @@ pub struct CacheStat {
 
 impl Display for CacheStat {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        writeln!(f, "{} lines x 4 words direct-mapped cache", CACHE_LINE >> 2)?;
+        writeln!(f, "{} lines x 4 words direct-mapped cache", CACHE_LINE)?;
         writeln!(
             f,
             "Read: {}, Hit: {} ({:.02}%), Miss: {} ({:.02}%)",
@@ -89,7 +89,7 @@ impl Display for CacheStat {
     }
 }
 
-pub const CACHE_LINE_BITS: usize = 16;
+pub const CACHE_LINE_BITS: usize = 14;
 pub const CACHE_LINE: usize = 1 << CACHE_LINE_BITS;
 pub const MEMORY_SIZE: usize = 1 << 19;
 pub const CACHE_MASK: usize = CACHE_LINE - 1;
