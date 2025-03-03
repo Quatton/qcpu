d9015b196f0cf252456a5d999827dc1ce86a7c9e
Quatton <atomicatives@gmail.com>
2025-03-03 23:00:20 +0900

    run script

--- run.sh
+++ run.sh
@@ -4,7 +4,13 @@
 for i in {1..3}
 do
   (
-   cargo run --release --quiet sim -b "./test_data/minrt_$((128 * 2**(i-1))).bin" --verbose
+   num=$((128 * 2**(i-1)))
+   qcpu sim -b "./test_data/minrt_${num}.bin" --verbose
+   output_file="output${i}.txt"
+   reference_file="minrt_${num}.ppm"
+   if [ -f "$output_file" ]; then
+     qcpu diff -s "$reference_file" -t "$output_file"
+   fi
   ) &
 done
 wait
